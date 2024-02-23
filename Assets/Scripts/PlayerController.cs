f0e436d566b6d94ad84c923433bd1565538713c5
Duvan R <dumirola.dr@gmail.com>
2024-02-23 13:01:11 -0500

    UI canvas, select dance

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Animator animator;
+    [SerializeField] private Button Select_Dance_1;
+    [SerializeField] private Button Select_Dance_2;
+    [SerializeField] private Button Select_Dance_3;
+    void Start()
+    {
+        GameObject player = GameObject.Find("player");
+
+          animator = player.GetComponent<Animator>();
+    }
+
+    public void Dance_1_Selected()
+    {
+        
+        if (animator != null)
+        {
+            animator.SetBool("b_House_Dance", false);
+            animator.SetBool("b_HipHop_Dance", true);
+            animator.SetBool("b_Macarena_Dance", false);
+            
+        }
+    }
+   public void Dance_2_Selected()
+    { 
+        if (animator != null)
+        {
+            animator.SetBool("b_House_Dance", false);
+            animator.SetBool("b_HipHop_Dance", false);
+            animator.SetBool("b_Macarena_Dance", true);
+            
+        }
+    }
+    public void Dance_3_Selected()
+    {
+        if (animator != null)
+        {
+            animator.SetBool("b_House_Dance", true);
+            animator.SetBool("b_HipHop_Dance", false);
+            animator.SetBool("b_Macarena_Dance", false);
+        }
+         
+    }
+
+}
